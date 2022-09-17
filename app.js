7849f06bca1a14ef63097692bac4be256636ee5f
Pratt <pratt.paredes@gmail.com>
2022-09-17 00:37:32 -0500

    sorting API data with async/await and filter()

--- app.js
+++ app.js
@@ -78,8 +78,9 @@ console.log(
 async function postsByUser(userId) {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts')
     const data = await response.json()
-    console.log(data)
+    const posts = data.filter(element => element.userId === userId)
+    console.log(posts)
 }
 
 
-postsByUser(4)
+console.log(postsByUser(4))
